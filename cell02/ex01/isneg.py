17d544c32228a9e91034bec178f4768dc2cc5f7d
68160016-crypto <68160016@kmitl.ac.th>
2025-11-03 14:12:46 +0700

    isneg.py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,12 @@
+number = int(input("Enter a number: "))
+if number < 0:
+    print("This number is negative.")
+elif number > 0:
+    print("This number is positive.")
+else:
+    print("This number is both positive and negative.")
+ผลลัพธ์
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+Enter a number: -423
+This number is negative.
+PS C:\Users\user\.vscode>
