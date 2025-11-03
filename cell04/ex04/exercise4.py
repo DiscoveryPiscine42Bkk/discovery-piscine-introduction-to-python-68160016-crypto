77c554df84b4b9685799011ee273f30464bc070a
68160016-crypto <68160016@kmitl.ac.th>
2025-11-03 19:51:03 +0700

    exercise4.py

---
+++ cell04/ex04/exercise4.py
@@ -0,0 +1,16 @@
+import math
+num = float(input("Give me a number: "))
+rounded = math.ceil(num)
+print(rounded)
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+Give me a number: 41.42
+41.42
+PS C:\Users\user\.vscode>
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+Give me a number: 42
+42
+PS C:\Users\user\.vscode>
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+Give me a number: 0.001
+1
+PS C:\Users\user\.vscode>
