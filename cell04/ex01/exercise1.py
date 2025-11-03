af5cf7896bb3a8a99d7e0e8858d17d6f5367c898
68160016-crypto <68160016@kmitl.ac.th>
2025-11-03 19:16:27 +0700

    exercise1.py

---
+++ cell04/ex01/exercise1.py
@@ -0,0 +1,12 @@
+age = int(input("Please tell me your age: "))
+print(f"You are currently {age} years old.")
+print(f"In 10 years, you'll be {age + 10} years old.")
+print(f"In 20 years, you'll be {age + 20} years old.")
+print(f"In 30 years, you'll be {age + 30} years old.")
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+Please tell me your age: 19
+You are currently 19 years old.
+In 10 years, you'll be 29 years old.
+In 20 years, you'll be 39 years old.
+In 30 years, you'll be 49 years old.
+PS C:\Users\user\.vscode>
