785632bcc7e731bcc069c9773508d22d79c02340
68160016-crypto <68160016@kmitl.ac.th>
2025-11-03 19:12:47 +0700

    exercise0.py

---
+++ cell04/ex00/exercise0.py
@@ -0,0 +1,6 @@
+word = input("Give me a word: ")
+print(word.upper())
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+Give me a word:banana
+BANANA
+PS C:\Users\user\.vscode>
