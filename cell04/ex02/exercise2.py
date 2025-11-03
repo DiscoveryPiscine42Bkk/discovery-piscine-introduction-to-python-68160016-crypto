62ebe9acf1731083b84b879d19cc1f36a6b78972
68160016-crypto <68160016@kmitl.ac.th>
2025-11-03 19:41:46 +0700

    exercise2.py

---
+++ cell04/ex02/exercise2.py
@@ -0,0 +1,16 @@
+num1 = float(input("Give me the first number: "))
+num2 = float(input("Give me the second number: "))
+print("Thank you!")
+print(f"{num1:g} + {num2:g} = {num1 + num2:g}")
+print(f"{num1:g} - {num2:g} = {num1 - num2:g}")
+print(f"{num1:g} / {num2:g} = {num1 / num2:g}")
+print(f"{num1:g} * {num2:g} = {num1 * num2:g}")
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+Give me the first number: 10
+Give me the second number: 2
+Thank you!
+10 + 2 = 12
+10 - 2 = 8
+10 / 2 = 5
+10 * 2 = 20
+PS C:\Users\user\.vscode>
