6e7bd47dbdf797e84352b54d30d828db6da7397f
68160016-crypto <68160016@kmitl.ac.th>
2025-11-03 13:48:51 +0700

    age.py

---
+++ cell01/ex02/age.py
@@ -0,0 +1,7 @@
+age = 19
+my_age = age + 42 
+print(my_age)
+ผลลัพธ์ 
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+61
+PS C:\Users\user\.vscode> 
