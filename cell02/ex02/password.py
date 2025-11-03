3b86485be7478b93ffb4f25629c683411835511b
68160016-crypto <68160016@kmitl.ac.th>
2025-11-03 14:21:34 +0700

    password.py

---
+++ cell02/ex02/password.py
@@ -0,0 +1,11 @@
+password = "Python is awesome"
+user_input = input("Enter the password: ")
+if user_input == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
+  ผลลัพธ์
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+Enter the password: Python is awesome
+ACCESS GRANTED
+PS C:\Users\user\.vscode>
