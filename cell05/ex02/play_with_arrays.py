2bab30d571d53873c7069a7af115b2430fcbd6cd
68160016-crypto <68160016@kmitl.ac.th>
2025-11-03 20:38:20 +0700

    play_with_arrays.py

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -2,3 +2,7 @@ original_array = [2, 8, 9, 48, 8, 22, -12, 2]
 new_array = [x + 2 for x in original_array if x > 5]
 print(original_array)
 print(new_array)
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+Original array: [2, 8, 9, 48, 8, 22, -12, 2]
+New array: [4, 10, 11, 50, 10, 24, -10, 4]
+PS C:\Users\user\.vscode>
