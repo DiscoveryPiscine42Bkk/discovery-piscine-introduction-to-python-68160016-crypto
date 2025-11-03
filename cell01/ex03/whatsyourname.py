da22e4da1357f1cdd91a265c51fbaa259279f884
68160016-crypto <68160016@kmitl.ac.th>
2025-11-03 13:57:09 +0700

    whatsyourname.py

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,9 @@
+first_name = input("Hey, what's your first name? : ")
+last_name = input("And your last name? : ")
+print(f"Well, pleased to meet you, {first_name} {last_name}.")
+ผลลัพธ์ 
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+Hey, what's your first name? : Tyler 
+And your last name? : Durden
+Well, pleased to meet you, T Durden.
+PS C:\Users\user\.vscode>
