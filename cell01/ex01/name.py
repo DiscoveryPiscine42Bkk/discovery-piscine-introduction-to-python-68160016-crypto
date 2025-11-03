da61317ef9a189d7e3603c13676d91a5afec376c
68160016-crypto <68160016@kmitl.ac.th>
2025-11-03 13:39:11 +0700

    name.py

---
+++ cell01/ex01/name.py
@@ -0,0 +1,8 @@
+first_name = "Wil"
+last_name = "42"
+whole_name = first_name + " " + last_name
+print(whole_name)
+ผลลัพธ์ PS C:
+\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+Wil 42
+PS C:\Users\user\.vscode>
