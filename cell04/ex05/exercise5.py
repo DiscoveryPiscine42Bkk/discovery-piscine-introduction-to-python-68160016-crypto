38fb0902f4ff995f1a2942bbe55fd3744b16442f
68160016-crypto <68160016@kmitl.ac.th>
2025-11-03 19:57:04 +0700

    exercise5.py

---
+++ cell04/ex05/exercise5.py
@@ -0,0 +1,7 @@
+text = input()
+swapped = text.swapcase()
+print(swapped)
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+hello42
+HELLO42
+PS C:\Users\user\.vscode>
