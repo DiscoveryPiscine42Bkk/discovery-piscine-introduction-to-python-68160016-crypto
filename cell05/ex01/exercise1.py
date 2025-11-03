8ac383df8bf18c547b1af7a2c61a1da225a45c84
68160016-crypto <68160016@kmitl.ac.th>
2025-11-03 20:11:08 +0700

    exercise1.py

---
+++ cell05/ex01/exercise1.py
@@ -0,0 +1,8 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = [x + 2 for x in original_array]
+print("Original array:", original_array)
+print("New array:", new_array)
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+Original array: [2, 8, 9, 48, 8, 22, -12, 2]
+New array: [4, 10, 11, 50, 10, 24, -10, 4]
+PS C:\Users\user\.vscode>
