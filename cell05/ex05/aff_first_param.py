1ce25f3ee7815a692292cb1099fc01fab63f88f4
68160016-crypto <68160016@kmitl.ac.th>
2025-11-03 21:50:45 +0700

    aff_first_param.py

--- cell05/ex05/aff_first_param.py
+++ cell05/ex05/aff_first_param.py
@@ -3,3 +3,11 @@ if len(sys.argv) > 1:
     print(sys.argv[1])
 else:
     print("none")
+    PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+    $ python aff_first_param.py | cat -e
+none$
+PS C:\Users\user\.vscode>
+ PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+$ python aff_first_param.py "Code Ninja" "Numerique" "42" | cat -e
+Code Ninja$
+PS C:\Users\user\.vscode>
