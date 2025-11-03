71f393fbfdb4cda261d96e7bb5b056ea30609340
68160016-crypto <68160016@kmitl.ac.th>
2025-11-03 20:57:12 +0700

    parameters.py

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -1,1 +1,12 @@
-
+import sys
+num_params = len(sys.argv) - 1
+print(f"Number of parameters: {num_params}.")
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+Number of parameters: 0.
+PS C:\Users\user\.vscode\ex04> python parameters.py
+Number of parameters: 0.
+PS C:\Users\user\.vscode\ex04> python parameters.py initiation
+Number of parameters: 1.
+PS C:\Users\user\.vscode\ex04> python parameters.py this is crazy "there's everywhere!"
+Number of parameters: 5.
+PS C:\Users\user\.vscode>
