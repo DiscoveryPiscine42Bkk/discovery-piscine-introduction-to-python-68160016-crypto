3c9f7ab9cdb9a7376601214205bed23b792613fd
68160016-crypto <68160016@kmitl.ac.th>
2025-11-03 14:04:54 +0700

    iszero.py

---
+++ cell02/ ex00/iszero.py
@@ -0,0 +1,10 @@
+number = int(input("Enter a number: "))
+if number == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
+  ผลลัพธ์ 
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+Enter a number: 0
+This number is equal to zero.
+PS C:\Users\user\.vscode>
