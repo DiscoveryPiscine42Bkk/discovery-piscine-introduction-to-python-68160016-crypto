ef603c117fda8114bd063be72206d7821eec3a69
68160016-crypto <68160016@kmitl.ac.th>
2025-11-04 07:37:25 +0700

    downcase_it.py

--- cell05/ex07/downcase_it.py
+++ cell05/ex07/downcase_it.py
@@ -3,14 +3,11 @@ if len(sys.argv) != 2:
     print("none")
 else:
     print(sys.argv[1].lower())
-PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/downcase_it.py
+ไม่มีพารามิเตอรฺ์ C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py This exercise is quite easy 
 none
-PS C:\Users\user\.vscode>
-
-PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/downcase_it.py "LUCIOLE"
+กรอก LUCIOLE 
+C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py LUCIOLE 
 luciole
-PS C:\Users\user\.vscode>
-
-PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/downcase_it.py "This exercise is quite easy!"
+กรอก This exercise is quite easy!
+C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py This exercise is quite easy!
 this exercise is quite easy!
-PS C:\Users\user\.vscode>
