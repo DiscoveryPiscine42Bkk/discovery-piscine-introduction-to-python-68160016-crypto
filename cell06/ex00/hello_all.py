17ab7fc869b20b333d305a7e27b1d302914249f1
68160016-crypto <68160016@kmitl.ac.th>
2025-11-04 08:24:04 +0700

    hello_all.py

--- cell06/ex00/hello_all.py
+++ cell06/ex00/hello_all.py
@@ -1,1 +1,6 @@
+def hello():
+    print("Hello, everyone!")
+hello()
+C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py 10 14 
+Hello, everyone!
 
