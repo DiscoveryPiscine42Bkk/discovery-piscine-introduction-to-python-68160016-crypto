630d2e90edb5ccdfc5d1de99ce29c618a2661e50
68160016-crypto <68160016@kmitl.ac.th>
2025-11-04 08:33:55 +0700

    parameters.py

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -1,7 +1,7 @@
 import sys
 num_params = len(sys.argv) - 1
 print(f"Number of parameters: {num_params}.")
-ตั้งแต่ข้อนี้เป็นต้นไปจะใช้ pycharm
+ตั้งแต่ข้อนี้เป็นต้นไปจะใช้ PyCharm เพราะมันใช้ง่าย และผลลัพธ์ก็เหมือนกับใช้ VS Code ทุกอย่าง
 PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/parameters.py
 Number of parameters: 0
 กรอกข้อมูล initiation 
