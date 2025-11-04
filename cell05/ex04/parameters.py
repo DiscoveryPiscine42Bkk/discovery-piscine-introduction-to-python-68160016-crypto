c7ade5599da86628559fe856394106ba09cd9c52
68160016-crypto <68160016@kmitl.ac.th>
2025-11-04 07:22:54 +0700

    parameters.py

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -1,13 +1,11 @@
 import sys
 num_params = len(sys.argv) - 1
 print(f"Number of parameters: {num_params}.")
-PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
-Number of parameters: 0.
-PS C:\Users\user\.vscode\ex04> python parameters.py
-Number of parameters: 0.
-PS C:\Users\user\.vscode\ex04> python parameters.py initiation
-Number of parameters: 1.
-PS C:\Users\user\.vscode\ex04> python parameters.py this is crazy "there's everywhere!"
-Number of parameters: 5.
-PS C:\Users\user\.vscode>
-ใน VS Code เราไม่กรอกค่าภายในตัวไฟล์ แต่ต้อง “ใส่ค่าพารามิเตอร์ด้านนอก” ตอนรันโปรแกรม เช่นใน Terminal ด้านล่างของ VS Code
+ตั้งแต่ข้อนี้เป็นต้นไปจะใช้ pycharm
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/parameters.py
+Number of parameters: 0
+กรอกข้อมูล initiation 
+C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py initiation 
+Number of parameters: 1
+กรอกข้อมูล  "this" "is" "crazy" "there's" "everywhere!"
+Number of parameters: 5
