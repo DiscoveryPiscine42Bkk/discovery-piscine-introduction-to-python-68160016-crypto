4cc831d525743e770febe2a38c421ceb99d208a3
68160016-crypto <68160016@kmitl.ac.th>
2025-11-04 08:25:55 +0700

    upcase_it.py

---
+++ cell06/ex01/upcase_it.py
@@ -0,0 +1,5 @@
+def upcase_it(text):
+    return text.upper() 
+print(upcase_it("hello"))
+C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py 10 14 
+HELLO
