d11c90aa3dbb5b0810761fa6c2a33d82a4430fa1
68160016-crypto <68160016@kmitl.ac.th>
2025-11-04 08:01:19 +0700

    count_it.py

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -4,13 +4,10 @@ if len(args) == 0:
     print("none")
 else:
     print(f"parameters: {len(args)}")
-    for arg in args:
-        print(f"{arg}: {len(arg)}")
-PS C:\Users\user\.vscode\ex11> & C:/Users/user/.vscode/.venv/Scripts/python.exe .\count_it.py
-none
-PS C:\Users\user\.vscode\ex11> & C:/Users/user/.vscode/.venv/Scripts/python.exe .\count_it.py "Game" "of" "Thrones"
+    for param in args:
+        print(f"{param}: {len(param)}")
+C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py Game of Thrones 
 parameters: 3
 Game: 4
 of: 2
 Thrones: 7
-PS C:\Users\user\.vscode\ex10>
