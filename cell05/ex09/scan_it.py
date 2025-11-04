1527aa6c5c7d34b8b3cbcc6f260fd01aa32664a1
68160016-crypto <68160016@kmitl.ac.th>
2025-11-04 07:52:23 +0700

    scan_it.py

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,20 +1,22 @@
 import sys
 args = sys.argv[1:]
-if len(args) != 2:
+if len(args) != 1:
     print("none")
 else:
-    keyword = args[0]
-    text = args[1]
-    count = text.count(keyword)
-    if count == 0:
-        print("none")
+    param = args[0]
+    user_input = input("What was the parameter? ")
+    if user_input == param:
+        print("Good job!")
     else:
-        print(count)
-    ไม่กรอกอะไร  C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py Python piscine hello 
+        print("Nope, sorry...")
+
+ไม่กรอก C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py 
 none
-กรอก the 
-C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py the 
-none
-กรอก the "the quick brown fox jumps over the lazy dog"
-C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py the "the quick brown fox jumps over the lazy dog" 
-2
+กรอก  Bonjour
+C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py "What was the parameter? " 
+What was the parameter? Bonjour
+Nope, sorry...
+กรอก Hello
+C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py Hello 
+What was the parameter? Hello
+Good job!
