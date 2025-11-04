c5dede4776fa6177058052b7c6fa361aa526c66b
68160016-crypto <68160016@kmitl.ac.th>
2025-11-04 07:58:53 +0700

    parameter_matching.py

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,18 +1,23 @@
 import sys
-if len(sys.argv) != 2:
+args = sys.argv[1:]
+if len(args) != 1:
     print("none")
 else:
-    word = input("What was the parameter? ")
-    if word == sys.argv[1]:
+    param = args[0]
+    user_input = input("What was the parameter? ")
+    if user_input == param:
         print("Good job!")
     else:
         print("Nope, sorry...")
-PS C:\Users\user\.vscode\ex10> & C:/Users/user/.vscode/.venv/Scripts/python.exe .\parameter_matching.py
+
+ไม่กรอก C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py 
 none
-PS C:\Users\user\.vscode\ex10> & C:/Users/user/.vscode/.venv/Scripts/python.exe .\parameter_matching.py "Hello"
+กรอก  Bonjour
+C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py "What was the parameter? " 
 What was the parameter? Bonjour
 Nope, sorry...
-PS C:\Users\user\.vscode\ex10> & C:/Users/user/.vscode/.venv/Scripts/python.exe .\parameter_matching.py "Hello"
+กรอก Hello
+C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py Hello 
 What was the parameter? Hello
 Good job!
-PS C:\Users\user\.vscode\ex10>
+
