f8725c258dbc7b0409bd81dae101c599a071d1fe
68160016-crypto <68160016@kmitl.ac.th>
2025-11-04 08:12:38 +0700

    string_are_arrays.py

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -1,21 +1,27 @@
 import sys
+
 args = sys.argv[1:]
 
 if len(args) != 1:
     print("none")
 else:
     text = args[0]
-    result = "".join([ch for ch in text if ch == "z"])
-    if result:
-        print(result)
-    else:
+
+    z_chars = [ch for ch in text if ch.lower() == 'z']
+
+    if len(z_chars) == 0:
         print("none")
-PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/string_are_arrays.py
+    else:
+        print("".join(['z' for _ in z_chars]))
+
+ไม่กรอก C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py 
 none
-PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/string_are_arrays.py "The character Z is not found in this string"
+ตัว Z 
+C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py The character Z is not found in this string 
 none
-PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/string_are_arrays.py "The character z is found in this string"
-z
-PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/string_are_arrays.py "Zaz visits the zoo with Zazie"
+ตัว z
+C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py The character z is found in this string 
+none
+Z หลายตัว "Zaz visits the zoo with Zazie"
+C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py "Zaz visits the zoo with Zazie" 
 zzz
-PS C:\Users\user\.vscode>
