596719d57152143e9db0998c42de1de29f29831f
68160016-crypto <68160016@kmitl.ac.th>
2025-11-04 10:04:23 +0700

    scope_that.py

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,12 @@
+global_var = "I am global!"
+def my_function():
+    local_var = "I am local!"
+    print(local_var)  
+    print(global_var)  
+my_function()
+print(global_var)
+# print(local_var)   
+C:\Users\user\PyCharmMiscProject\.venv\Scripts\python.exe C:\Users\user\PyCharmMiscProject\parameter.py lol physically backpack 
+I am local!
+I am global!
+I am global!
