ba6fa81dfc5f817660d1de87b0e9e9cac9c2282b
68160016-crypto <68160016@kmitl.ac.th>
2025-11-05 14:49:00 +0700

    hello_world.py

--- cell00/ex02/hello_world.py
+++ cell00/ex02/hello_world.py
@@ -1,6 +1,7 @@
+print("Hello World")
  PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
-print("Hello World$")
-Hello World$
+Hello World
 PS C:\Users\user\.vscode>
+
 
 
