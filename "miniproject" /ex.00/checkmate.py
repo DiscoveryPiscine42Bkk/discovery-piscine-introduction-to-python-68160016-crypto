1adf046a4e8a3c7436e43dc5fb19649bc31f04b9
68160016-crypto <68160016@kmitl.ac.th>
2025-11-05 16:35:09 +0700

    checkmate.py

--- "miniproject" /ex.00/checkmate.py
+++ "miniproject" /ex.00/checkmate.py
@@ -1,1 +1,44 @@
+def checkmate(board_str):
+    board = [list(line) for line in board_str.splitlines()]
+    size = len(board)
+    
+    # หา King
+    king_x, king_y = -1, -1
+    for i in range(size):
+        for j in range(size):
+            if board[i][j] == 'K':
+                king_x, king_y = i, j
+
+    # ตรวจสอบทุก piece
+    for i in range(size):
+        for j in range(size):
+            piece = board[i][j]
+
+            # Rook ตรวจแนวตั้ง-แนวนอน
+            if piece == 'R':
+                for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+                    x, y = i, j
+                    while 0 <= x+dx < size and 0 <= y+dy < size:
+                        x += dx
+                        y += dy
+                        if (x, y) == (king_x, king_y):
+                            print("Success")
+                            return
+                        if board[x][y] != '.':
+                            break
+
+            # Bishop ตรวจแนวทแยง
+            if piece == 'B':
+                for dx, dy in [(-1,-1), (-1,1), (1,-1), (1,1)]:
+                    x, y = i, j
+                    while 0 <= x+dx < size and 0 <= y+dy < size:
+                        x += dx
+                        y += dy
+                        if (x, y) == (king_x, king_y):
+                            print("Success")
+                            return
+                        if board[x][y] != '.':
+                            break
+
+    print("Fail")
 
