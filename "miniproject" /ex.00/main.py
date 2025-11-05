4a3dcbfd8f346013b98082fd791b07f4c89c552d
68160016-crypto <68160016@kmitl.ac.th>
2025-11-05 16:35:28 +0700

    main.py

--- "miniproject" /ex.00/main.py
+++ "miniproject" /ex.00/main.py
@@ -1,1 +1,15 @@
+from checkmate import checkmate
+
+def main():
+    # กระดาน 4x4 King ถูกโจมตีโดย Bishop
+    board = """\
+B...
+.K..
+....
+....\
+"""
+    checkmate(board)  # จะพิมพ์ "Success"
+
+if __name__ == "__main__":
+    main()
 
