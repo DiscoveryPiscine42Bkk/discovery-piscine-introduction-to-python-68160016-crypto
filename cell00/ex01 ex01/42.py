608b870063dee12b36c2ee21d9582c0784d0ccce
68160016-crypto <68160016@kmitl.ac.th>
2025-11-05 14:47:43 +0700

    42.py

--- cell00/ex01 ex01/42.py
+++ cell00/ex01 ex01/42.py
@@ -1,3 +1,5 @@
-PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
 print("42")
+ผลลัพธ์ 
+PS C:\Users\user\.vscode> & C:/Users/user/.vscode/.venv/Scripts/python.exe c:/Users/user/.vscode/hello_world.py
+42
 PS C:\Users\user\.vscode> 
