feb96269f4491fb65f147a3ab732b6947b1eff1d
68160016-crypto <68160016@kmitl.ac.th>
2025-11-05 19:10:51 +0700

    main.py

--- "miniproject" /ex.00/main.py
+++ "miniproject" /ex.00/main.py
@@ -1,11 +1,26 @@
-from checkmate import checkmate
+from checkmate import is_king_in_check
+
 def main():
-    board = """\
+    # ตัวอย่าง 1: Success (King ถูกโจมตี)
+    # B = Bishop, P = Pawn, K = King, . = ช่องว่าง
+    chess_board_success = """\
 ....
 .B..
 .PK.
-....\
+....
 """
-    checkmate(board)  
+    print("Test 1 (King โดนโจมตี):")
+    is_king_in_check(chess_board_success)  # ควรพิมพ์ Success
+
+    print("\nTest 2 (King ปลอดภัย):")
+    # ตัวอย่าง 2: Fail (King ปลอดภัย)
+    chess_board_fail = """\
+....
+....
+..K.
+....
+"""
+    is_king_in_check(chess_board_fail)  # ควรพิมพ์ Fail
+
 if __name__ == "__main__":
     main()
