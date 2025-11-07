e90b7fb57ed3ad34fa4dddbc642d2c54cd04d383
68160016-crypto <68160016@kmitl.ac.th>
2025-11-07 09:50:52 +0700

    main.py

--- "miniproject" /ex.00/main.py
+++ "miniproject" /ex.00/main.py
@@ -1,6 +1,7 @@
 from checkmate import checkmate
 
 def main():
+    # Test 1: King โดนโจมตี
     chess_board_success = """\
 ....
 .B..
@@ -8,15 +9,17 @@ def main():
 ....\
 """
     print("Test 1 (King โดนโจมตี):")
-    checkmate(chess_board_success)  
-    print("\nTest 2 (King ปลอดภัย):")
+    checkmate(chess_board_success)
+
+    # Test 2: King ปลอดภัย
     chess_board_fail = """\
 R...
 ....
 ..K.
 ....\
 """
-    checkmate(chess_board_fail)  
+    print("\nTest 2 (King ปลอดภัย):")
+    checkmate(chess_board_fail)
 
 if __name__ == "__main__":
     main()
